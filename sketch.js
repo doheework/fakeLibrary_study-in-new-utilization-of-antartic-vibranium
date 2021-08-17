6ebb1b0af1eedb3ed6ef60ba9852a63a172c3808
Your Namedo <you@example.com>
2021-08-18 00:57:50 +0900

    18.08.1

---
+++ sketch.js
@@ -0,0 +1,340 @@
+$(document).ready(function(){
+    $(".숙제").mouseover(function(){
+        var image = $(".숙제i");
+    $(".숙제i").show();
+    $(".숙제i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".숙제").mouseout(function(){
+    $(".숙제i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".세권").mouseover(function(){
+        var image = $(".세권i");
+    $(".세권i").show();
+    $(".세권i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".세권").mouseout(function(){
+    $(".세권i").stop().animate({right:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".콜로서스").mouseover(function(){
+        var image = $(".콜로서스i");
+    $(".콜로서스i").show();
+    $(".콜로서스i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".콜로서스").mouseout(function(){
+    $(".콜로서스i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".콜로서스2").mouseover(function(){
+        var image = $(".콜로서스2i");
+    $(".콜로서스2i").show();
+    $(".콜로서스2i").stop().animate({top:20});
+    });
+    $(".콜로서스2").mouseout(function(){
+    $(".콜로서스2i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".금속").mouseover(function(){
+        var image = $(".금속i");
+    $(".금속i").show();
+    $(".금속i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".금속").mouseout(function(){
+    $(".금속i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".남극").mouseover(function(){
+        var image = $(".남극i");
+    $(".남극i").show();
+    $(".남극i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".남극").mouseout(function(){
+    $(".남극i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".울버린").mouseover(function(){
+        var image = $(".울버린i");
+    $(".울버린i").show();
+    $(".울버린i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".울버린").mouseout(function(){
+    $(".울버린i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".토르").mouseover(function(){
+        var image = $(".토르i");
+    $(".토르i").show();
+    $(".토르i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".토르").mouseout(function(){
+    $(".토르i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".유전자").mouseover(function(){
+        var image = $(".유전자i");
+    $(".유전자i").show();
+    $(".유전자i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".유전자").mouseout(function(){
+    $(".유전자i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".비브라늄").mouseover(function(){
+        var image = $(".비브라늄i");
+    $(".비브라늄i").show();
+    $(".비브라늄i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".비브라늄").mouseout(function(){
+    $(".비브라늄i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".엑스맨").mouseover(function(){
+        var image = $(".엑스맨i");
+    $(".엑스맨i").show();
+    $(".엑스맨i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".엑스맨").mouseout(function(){
+    $(".엑스맨i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".체르노빌").mouseover(function(){
+        var image = $(".체르노빌i");
+    $(".체르노빌i").show();
+    $(".체르노빌i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".체르노빌").mouseout(function(){
+    $(".체르노빌i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".찰스").mouseover(function(){
+        var image = $(".찰스i");
+    $(".찰스i").show();
+    $(".찰스i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".찰스").mouseout(function(){
+    $(".찰스i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".방패").mouseover(function(){
+        var image = $(".방패i");
+    $(".방패i").show();
+    $(".방패i").stop().animate({top:-200});
+    });
+    $(".방패").mouseout(function(){
+    $(".방패i").stop().animate({top:"600%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".밀론").mouseover(function(){
+        var image = $(".밀론i");
+    $(".밀론i").show();
+    $(".밀론i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".밀론").mouseout(function(){
+    $(".밀론i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".상처").mouseover(function(){
+        var image = $(".상처i");
+    $(".상처i").show();
+    $(".상처i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".상처").mouseout(function(){
+    $(".상처i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".울트론").mouseover(function(){
+        var image = $(".울트론i");
+    $(".울트론i").show();
+    $(".울트론i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".울트론").mouseout(function(){
+    $(".울트론i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".블랙팬서").mouseover(function(){
+        var image = $(".블랙팬서i");
+    $(".블랙팬서i").show();
+    $(".블랙팬서i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".블랙팬서").mouseout(function(){
+    $(".블랙팬서i").stop().animate({right:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".전투기").mouseover(function(){
+        var image = $(".전투기i");
+    $(".전투기i").show();
+    $(".전투기i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".전투기").mouseout(function(){
+    $(".전투기i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".호크아이").mouseover(function(){
+        var image = $(".호크아이i");
+    $(".호크아이i").show();
+    $(".호크아이i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".호크아이").mouseout(function(){
+    $(".호크아이i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".트라이").mouseover(function(){
+        var image = $(".트라이i");
+    $(".트라이i").show();
+    $(".트라이i").stop().animate({top:20});
+    });
+    $(".트라이").mouseout(function(){
+    $(".트라이i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".스파이더맨").mouseover(function(){
+        var image = $(".스파이더맨i");
+    $(".스파이더맨i").show();
+    $(".스파이더맨i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".스파이더맨").mouseout(function(){
+    $(".스파이더맨i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".센티넬").mouseover(function(){
+        var image = $(".센티넬i");
+    $(".센티넬i").show();
+    $(".센티넬i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".센티넬").mouseout(function(){
+    $(".센티넬i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".바이오닉").mouseover(function(){
+        var image = $(".바이오닉i");
+    $(".바이오닉i").show();
+    $(".바이오닉i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".바이오닉").mouseout(function(){
+    $(".바이오닉i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".미스티").mouseover(function(){
+        var image = $(".미스티i");
+    $(".미스티i").show();
+    $(".미스티i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".미스티").mouseout(function(){
+    $(".미스티i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".남극2").mouseover(function(){
+        var image = $(".남극2i");
+    $(".남극2i").show();
+    $(".남극2i").stop().animate({top:window.innerHeight/2-image.innerHeight()/2});
+    });
+    $(".남극2").mouseout(function(){
+    $(".남극2i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".바보").mouseover(function(){
+        var image = $(".바보i");
+    $(".바보i").show();
+    $(".바보i").stop().animate({top:window.innerHeight-image.innerHeight()+5});
+    });
+    $(".바보").mouseout(function(){
+    $(".바보i").stop().animate({top:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".책등").mouseover(function(){
+        var image = $(".책등i");
+    $(".책등i").show();
+    $(".책등i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".책등").mouseout(function(){
+    $(".책등i").stop().animate({right:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".제목").mouseover(function(){
+        var image = $(".제목i");
+    $(".제목i").show();
+    $(".제목i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".제목").mouseout(function(){
+    $(".제목i").stop().animate({right:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".파란").mouseover(function(){
+        var image = $(".파란i");
+    $(".파란i").show();
+    $(".파란i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".파란").mouseout(function(){
+    $(".파란i").stop().animate({right:"100%"});
+    });
+    });
+
+$(document).ready(function(){
+    $(".그림").mouseover(function(){
+        var image = $(".그림i");
+    $(".그림i").show();
+    $(".그림i").stop().animate({right:window.innerWidth-image.innerWidth()});
+    });
+    $(".그림").mouseout(function(){
+    $(".그림i").stop().animate({right:"100%"});
+    });
+    });
